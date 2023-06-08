2397a377520f26f0694d29baa5a8d114e48ebcd6
Mario Vieilledent <mario.vieilledent@gmail.com>
2023-06-08 15:04:37 +0200

    Débuts

---
+++ REAMDE.md
@@ -0,0 +1,10 @@
+# DirtyPazu-Server-v2
+
+Serveur v2 du dictionnaire de la langue Dibi.
+
+Ce projet est la v2 du dictionnaire en ligne, le [premier dictionnaire](https://dibi-dictionary.onrender.com/) reste disponible et fonctionnel.
+
+Les objectifs de cette seconde version :
+1) Offrir à tous la possibilité de faire des propositions de mots directement sur le site afin d'automatiser et donc de grandement fluidifier l'ajout de mots.
+2) Offrir une application et une API 100% open-source
+3) Améliorer les performances du la web app en utilisant Svelte
--- server.js
+++ server.js
@@ -1,51 +1,92 @@
-import express from 'express';
-import axios from 'axios';
-import dotenv from 'dotenv';
+const express = require('express');
+const cors = require('cors')
+const axios = require('axios');
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const DEBUG_DICT = require('./dict.json');
+
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
-
 const app = express();
-
 app.set(PORT, process.env.PORT);
 app.use(express.static('./dist/'));
+app.use(cors());
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
-app.get('/auth', (req, res) => {
-  const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=identify`;
-  res.redirect(discordAuthUrl);
+// MongoDB
+const uri = `mongodb+srv://DirtyPazuv2:${process.env.MONGO_PWD}@pazucluster.klrce.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+const clusterName = 'PazuSarl';
 
+const collectionName = 'DibiDictonary';
+const collectionSuggestionName = 'Suggestion'
+const collectionProfilesName = 'Profiles'
+const collectionLogsName = 'logsDirtyPazu';
 
-app.get('/auth', (req, res) => {
-  const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=identify`;
-  res.redirect(discordAuthUrl);
+/*
+client.connect(async (err) => {
+  if (err) throw err;
 });
+*/
+
+// Discord OAuth
+app.use('/auth', express.static('./dist/'));
 
 app.get('/exchange-code', async (req, res) => {
-  const code = req.query.code;
-
-  const tokenEndpoint = 'https://discord.com/api/oauth2/token';
-  const data = new URLSearchParams();
-  data.append('client_id', CLIENT_ID);
-  data.append('client_secret', CLIENT_SECRET);
-  data.append('grant_type', 'authorization_code');
-  data.append('code', code);
-  data.append('redirect_uri', REDIRECT_URI);
-
   try {
-    const response = await axios.post(tokenEndpoint, data);
-    const { access_token: accessToken } = response.data;
+    const response = await axios.post(
+      'https://discord.com/api/oauth2/token',
+      new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        scope: 'identify email',
+        code: req.query.code,
+        redirect_uri: process.env.REDIRECT_URI,
+      }).toString(),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
 
-    // Use the access token as needed
-    res.send(accessToken);
+    // Use the response data as needed
+    const accessToken = response.data;
+    console.log(accessToken);
+
+    const userResponse = await axios.get('https://discord.com/api/users/@me', {
+      headers: {
+        Authorization: `Bearer ${accessToken.access_token}`,
+      },
+    });
+
+    // Use the user data as needed
+    res.send(userResponse.data);
   } catch (error) {
-    console.error('Error exchanging code for access token:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error exchanging authorization code:', error);
+    res.status(500).send('An error occurred while exchanging the authorization code.');
   }
+});
+
+// API of Dictionary
+app.get('/dict', async (req, res) => {
+  console.log('/dict, récupération du dictionnaire');
+  /*
+  try {
+    data = await client.db(clusterName).collection(collectionName).find().toArray();
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+  }
+  */
+  res.send(DEBUG_DICT);
 });
 
 app.listen(PORT, () => {
