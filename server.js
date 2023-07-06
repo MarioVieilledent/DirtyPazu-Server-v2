fd698e4d1f23777a83c99c3e5199361fd8cc5bd2
Mario <mario.vieilledent@gmail.com>
2023-07-06 16:47:22 +0200

    Unstable

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors')
 const axios = require('axios');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const DEBUG_DICT = require('./dict.json');
 
@@ -10,8 +12,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.set(PORT, process.env.PORT);
-app.use(express.static('./dist/'));
-app.use(cors());
+app.use(bodyParser.json()); // For parsing body data
+app.use(express.static('./dist/')); // Serving static web app
+app.use(cors()); // Allow CORS
 
 // MongoDB
 const uri = `mongodb+srv://DirtyPazuv2:${process.env.MONGO_PWD}@pazucluster.klrce.mongodb.net/?retryWrites=true&w=majority`;
@@ -81,10 +84,10 @@ app.get('/exchange-code', async (req, res) => {
         userConnected = user; // Gathering all data from user
       }
     });
-    
+
     if (alreadyExist) { // User already exist
       console.log('already exist');
-      await client.db(clusterName).collection(collectionDiscordProfilesName).updateOne({ 'discord.id': userConnected.discord.id }, { $push: { connections: Date.now() }, $inc: {xp: 0.1} })
+      await client.db(clusterName).collection(collectionDiscordProfilesName).updateOne({ 'discord.id': userConnected.discord.id }, { $push: { connections: Date.now() }, $inc: { xp: 0.1 } })
 
     } else { // If new user
       console.log('new user, profile creation');
@@ -106,7 +109,7 @@ app.get('/exchange-code', async (req, res) => {
   }
 });
 
-// API of Dictionary
+// Get all dictionary
 app.get('/dict', async (req, res) => {
   console.log('/dict, fetching dictionary');
   try {
@@ -115,6 +118,53 @@ app.get('/dict', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.send(DEBUG_DICT);
+  }
+});
+
+// Get data of a single word by an _id
+app.get('/wordId', async (req, res) => {
+  console.log('/wordId');
+  const id = new ObjectId(req.query.id);
+  console.log(id);
+  if (id) {
+    try {
+      data = await client.db(clusterName).collection(collectionDictionaryName).findOne({ _id: id });
+      res.send(data);
+    } catch (err) {
+      console.error(err);
+      res.send({ err: "No id matching: " + err });
+    }
+  } else {
+    res.send({ err: "Need id in query param" })
+  }
+});
+
+// Get all suggestions
+app.get('/suggestions', async (req, res) => {
+  console.log('/suggestions, fetching suggestions');
+  try {
+    data = await client.db(clusterName).collection(collectionSuggestionName).find().toArray();
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.send(DEBUG_DICT);
+  }
+});
+
+// Send a new suggestion of words
+app.post('/suggest', async (req, res) => {
+  console.log('Ajout d\'une suggestion');
+  const suggestion = req.body;
+  if (suggestion && suggestion.author && suggestion.versions.length > 0) {
+    try {
+      client.db(clusterName).collection(collectionSuggestionName).insertOne(suggestion);
+      res.send('"ok"');
+    } catch (e) {
+      console.error(err);
+      res.send({ err: "Error while suggesting: " + err });
+    }
+  } else {
+    res.send({ err: "Incorrect body input data" });
   }
 });
 
